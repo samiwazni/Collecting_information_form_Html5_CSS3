9d31c512e02447075fdabc3b4ab7a9b3c80a05ee
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2021-06-22 08:26:04 +0300

    Add files via upload

---
+++ CI_form.php
@@ -0,0 +1,16 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $number = $_POST['number'];
+    $message = $_POST['message'];
+    
+
+    $mailTo = "sami.wazni@hotmail.com";
+    $headers = "Form: ".$mailFrom;
+    $txt = "You have received an e-mail form ".$name.".\n\n".$message;
+
+    mail($mailTo,  $number, $txt, $headers);
+    header("Location: index.php\mailsend");
+}
